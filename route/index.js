d1df5cb2a5657f6869be44fdf94b32c6a9d91de8
洛音轩(YinxuanLuo） <34392070+YinxuanLuo@users.noreply.github.com>
2025-10-15 11:21:09 +0800

    Update index.js

--- route/index.js
+++ route/index.js
@@ -1,5 +1,5 @@
 const express = require('express');
-const mysql = require('mysql');
+const mysql = require('mysql2');
 const common = require('../libs/common');
 const db = mysql.createPool({
     host: 'localhost',
